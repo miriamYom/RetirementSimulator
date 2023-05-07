99022e4f293cd5855ecbe3c3a1a962c92fc9d690
miriamYom <m0527669018@gmail.com>
2023-05-07 21:20:16 +0300

    מידע כללי על עובד רגיל מוכן😒- הפרצוף הזה בגלל שעבדתי על זה יותר מדי זמן

--- client/src/RetirementSimulator/UserSee/GeneralData.js
+++ client/src/RetirementSimulator/UserSee/GeneralData.js
@@ -1,32 +1,146 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form"
 
 function GeneralData() {
+    // לבדוק על קינון קומפוננטות אמור להיות קליל
+
+    let months =
+        <>
+            <option value="January">ינואר</option>
+            <option value="February">פברואר</option>
+            <option value="March">מרץ</option>
+            <option value="April">אפריל</option>
+            <option value="May">מאי</option>
+            <option value="June">יוני</option>
+            <option value="July">יולי</option>
+            <option value="August">אוגוסט</option>
+            <option value="September">ספטמבר</option>
+            <option value="October">אוקטובר</option>
+            <option value="November">נובמבר</option>
+            <option value="December">דצמבר</option>
+        </>
+
     const { register, handleSubmit } = useForm();
+    const [isDismissal, setIsDismissal] = useState(true);
+    const [reason, setReason] = useState('retirementAge');
+    const [isClothingForAudienceMembers, setIsClothingForAudienceMembers] = useState(false);
+    const [isMonthlyClothingPayment, setIsMonthlyClothingPayment] = useState(false);
+    const [isThreeLevel, setIsThreeLevel] = useState(false);
+    const [isCurrentYear, setIsCurrentYear] = useState(false);
+    const [isMonthlyRecoveryPayment, setIsMonthlyRecoveryPayment] = useState(false);
+
+    const clothingForAudienceMembers = e => {
+        setIsClothingForAudienceMembers(e.target.value);
+    }
+
+    const monthlyClothingPayment = e => {
+        setIsMonthlyClothingPayment(e.target.value);
+    }
+
+    const threeLevel = e => {
+        setIsThreeLevel(e.target.value);
+    }
+
+    const Reason = e => {
+        setReason(e.target.value);
+    }
+
+    const currentYear = e => {
+        setIsCurrentYear(e.target.value);
+    }
+
+    const monthlyRecoveryPayment = e => {
+        setIsMonthlyRecoveryPayment(e.target.value);
+    }
 
     return (
-
         <>
             <center>
                 <input name="name" {...register('name')} placeholder="שם העובד"></input>
                 <br></br>
                 <input name="id" {...register('id')} placeholder="תעודת זהות"></input>
-                <br></br>
-                <input name="birthDate" {...register('birthDate')} placeholder="תאריך לידה"></input>
-                <br></br>
-                <input name="startWorkDate" {...register('startWorkDate')} placeholder="תאריך תחילת עבודה" type="date"></input>
-                <br></br>
-                <input name="retirementDate" {...register('retirementDate')} placeholder="תאריך פרישה צפוי" type="date"></input>
-                <br></br>
-                <label>סוג הפרישה</label>
-                <select name="reason" {...register("reason")}>
+                <h4>תאריך לידה</h4>
+                <input name="birthDate" {...register('birthDate')} type="date"></input>
+                <h4>תאריך תחילת עבודה</h4>
+                <input name="startWorkDate" {...register('startWorkDate')} type="date"></input>
+                <h4>תאריך פרישה צפוי</h4>
+                <input name="retirementDate" {...register('retirementDate')} type="date"></input>
+                <h4>סוג הפרישה</h4>
+                <select name="reason" {...register("reason")}
+                    onChange={Reason}>
                     <option value="retirementAge">גיל פרישה</option>
                     <option value="resignation">התפטרות</option>
                     <option value="dismissal">פיטורין</option>
                     <option value="retirementForHealthReasons">סיבה רפואית</option>
                     <option value="death">מוות</option>
                 </select>
+
                 <br></br>
+                {reason === "dismissal" ?
+                    (<>
+                        <h4>חלף הודעה מוקדמת</h4>
+                        <select name="advanceNotice" {...register("advanceNotice")}>
+                            <option value="notEligible">לא זכאי</option>
+                            <option value="month">חודש</option>
+                            <option value="twoMonths">חודשיים</option>
+                            <option value="treeMonths">שלושה חודשים</option>
+                        </select>
+                    </>) : null}
+                <h1>ביגוד</h1>
+                <h4>זכאות העובד לביגוד</h4>
+                <input type="radio" value="false" name="isClothingForAudienceMembers" {...register('isClothingForAudienceMembers')} onChange={clothingForAudienceMembers} /> ביגוד למקבלי קהל
+                <input type="radio" value="true" name="isClothingForAudienceMembers" {...register('isClothingForAudienceMembers')} onChange={clothingForAudienceMembers} /> ביגוד לפועלים
+
+                {isClothingForAudienceMembers === "true" ?
+                    (<>
+                        <br></br>
+                        <h4>אופן תשלום הביגוד</h4>
+                        <input type="radio" value="false" name="isMonthlyClothingPayment" {...register('isMonthlyClothingPayment')} onChange={monthlyClothingPayment} /> חודשי
+                        <input type="radio" value="true" name="isMonthlyClothingPayment" {...register('isMonthlyClothingPayment')} onChange={monthlyClothingPayment} /> שנתי
+                        <h4>רמת הביגוד</h4>
+                        <input type="radio" value="false" name="IsThreeLevel" {...register('IsThreeLevel')} onChange={threeLevel} /> רמה 3
+                        <input type="radio" value="true" name="IsThreeLevel" {...register('IsThreeLevel')} onChange={threeLevel} /> רמה 4
+                    </>) : null}
+                {isMonthlyClothingPayment === "false" ?
+                    (<>
+                        <h4>חודש תשלום הביגוד</h4>
+                        <select name="monthOfClothingPayment" {...register("monthOfClothingPayment")}>
+                            {months}
+                        </select>
+                    </>) : null}
+
+                {isClothingForAudienceMembers === "true" ?
+                    (<>
+                        <h4>השנה עבורה משולם הביגוד</h4>
+                        <input type="radio" value="true" name="IsCurrentYear" {...register('IsCurrentYear')} onChange={currentYear} /> שנה קנדרית קודמת
+                        <input type="radio" value="false" name="IsCurrentYear" {...register('IsCurrentYear')} onChange={currentYear} /> שנה נוכחית
+                    </>) : null}
+
+                <h1>הבראה</h1>
+                <h4>אופן תשלום ההבראה</h4>
+                <input type="radio" value="true" name="IsMonthlyRecoveryPayment" {...register('IsMonthlyRecoveryPayment')} onChange={monthlyRecoveryPayment} /> תשלום שנתי
+                <input type="radio" value="false" name="IsMonthlyRecoveryPayment" {...register('IsMonthlyRecoveryPayment')} onChange={monthlyRecoveryPayment} /> תשלום חודשי
+                {console.log("תשלום חודשי ", isMonthlyRecoveryPayment)}
+
+                <h4>מספר ימי הבראה לתשלום</h4>
+                <select name="numberOfDaysOfRecoveryToBePaid" {...register("numberOfDaysOfRecoveryToBePaid")}>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                </select>
+
+                {isMonthlyRecoveryPayment === "false" ?
+                    (<>
+                        <h4>חודש תשלום ההבראה</h4>
+                        <select name="recoveryPaymentMonth" {...register("recoveryPaymentMonth")}>
+                            {months}
+                        </select>
+                    </>) : null}
             </center>
+
         </>
     )
 }
