8818ada6abe31757831a8fb9ddd8254c93ed3864
miriamYom <m0527669018@gmail.com>
2023-05-07 16:48:20 +0300

    את חמודה מאוד

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import CreateEmployee from './RetirementSimulator/CreateEmployee';
 import BudgetPensin from './RetirementSimulator/UserSee/BudgetPension';
 import AccrualPension from './RetirementSimulator/UserSee/AccrualPension';
 import BudgetPensionForSeniorSalary from './RetirementSimulator/UserSee/BudgetPensionForSeniorSalary';
+import GeneralData from './RetirementSimulator/UserSee/GeneralData';
 
 
 function App() {
@@ -15,13 +16,14 @@ function App() {
      <BrowserRouter>
      {/* <Api></Api> */}
      {/* <CreateEmployee></CreateEmployee> */}
+     
         <Routes>
         <Route exact path="/" element={<LoginPage></LoginPage>}/>
          <Route exact path="ScreenOne" element={<ScreenOne></ScreenOne>}></Route>
          <Route exact path='BudgetPensin' element = {<BudgetPensin></BudgetPensin>}></Route>
          <Route exact path='AccrualPension' element = {<AccrualPension></AccrualPension>}></Route>
          <Route exact path='BudgetPensionForSeniorSalary' element = {<BudgetPensionForSeniorSalary></BudgetPensionForSeniorSalary>}></Route>
-         
+         <Route exact path='GeneralData' element = {<GeneralData></GeneralData>}></Route>
          </Routes>
       </BrowserRouter>
 
--- client/src/RetirementSimulator/UserSee/GeneralData.js
+++ client/src/RetirementSimulator/UserSee/GeneralData.js
@@ -1,14 +1,32 @@
-import { useState } from "react";
-import {useForm} from "react-hook-form"
+import { useForm } from "react-hook-form"
 
-function GeneralData(){
-    const [name, setName] = useState("");
+function GeneralData() {
     const { register, handleSubmit } = useForm();
 
-    return(
+    return (
 
         <>
-        <label>שם העובד</label>
+            <center>
+                <input name="name" {...register('name')} placeholder="שם העובד"></input>
+                <br></br>
+                <input name="id" {...register('id')} placeholder="תעודת זהות"></input>
+                <br></br>
+                <input name="birthDate" {...register('birthDate')} placeholder="תאריך לידה"></input>
+                <br></br>
+                <input name="startWorkDate" {...register('startWorkDate')} placeholder="תאריך תחילת עבודה" type="date"></input>
+                <br></br>
+                <input name="retirementDate" {...register('retirementDate')} placeholder="תאריך פרישה צפוי" type="date"></input>
+                <br></br>
+                <label>סוג הפרישה</label>
+                <select name="reason" {...register("reason")}>
+                    <option value="retirementAge">גיל פרישה</option>
+                    <option value="resignation">התפטרות</option>
+                    <option value="dismissal">פיטורין</option>
+                    <option value="retirementForHealthReasons">סיבה רפואית</option>
+                    <option value="death">מוות</option>
+                </select>
+                <br></br>
+            </center>
         </>
     )
 }
