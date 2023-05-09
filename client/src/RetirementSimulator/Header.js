6af7c789ad47a5064c9153c913d04ab7aae5291e
YehuditShrg <y0548562311@outlook.co.il>
2023-05-09 22:59:48 +0300

    header ia almost cpmplete 😃

--- client/src/RetirementSimulator/Header.js
+++ client/src/RetirementSimulator/Header.js
@@ -1,8 +1,7 @@
 import './Header.css';
 import logo from "../img/לבן לוגו מיכל לוי (1) 2.png";
 import phoneIcon from "../img/phone.png";
-import personIcon1 from "../img/person (1).png";
-import personIcon2 from "../img/person (2).png";
+import personIcon from "../img/person.png";
 function Header() {
 
     return (
@@ -10,22 +9,19 @@ function Header() {
             <div className="rec79"></div>
 
             <div className="frame39">
-                <a className="click-to-home-web" href=''>
+                <a className="click-to-home-web" href='https://michallcpa.co.il/'>
                     מעבר לדף הבית
                 </a>
             </div>
             <div className="group-289328">
-                <p className="welcom">היי { }</p>
-                <div className='group-welcom'>
-                    <img className='personIcon1' src={personIcon1}></img>
-                    <img className='personIcon2' src={personIcon2}></img>
-                </div>
+
+                <p className="welcome">
+                    <img className='personIcon' src={personIcon}></img>
+                    היי someone</p>
             </div>
             <div className="group-289327">
-                <div className='group'>
-                    <img className="vector-phone" src={phoneIcon}></img>
-                </div>
-                <p className="contact-us"> צור קשר </p>
+                <img className="vector-phone" src={phoneIcon}></img>
+                <p className="contact-us">צור קשר</p>
             </div>
             <img className="logo-header" src={logo}></img>
             <div className="line11"></div>
