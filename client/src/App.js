0d33fdafd73a9435c39be4ce3d4514482f8daa8e
YehuditShrg <y0548562311@outlook.co.il>
2023-05-09 13:33:58 +0300

    בס"ד

--- client/src/App.js
+++ client/src/App.js
@@ -9,22 +9,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import BudgetPensionForSeniorSalary from './RetirementSimulator/UserSee/BudgetPensionForSeniorSalary';
 // import GeneralData from './RetirementSimulator/UserSee/GeneralData';
 import Header from './RetirementSimulator/Header';
-
 function App() {
   return (
-    <div className='App-header'>
+    <div>
      <BrowserRouter>
      {/* <Api></Api> */}
      {/* <CreateEmployee></CreateEmployee> */}
      <Header></Header>
-        <Routes>
-        {/* <Route exact path="/" element={<LoginPage></LoginPage>}/>
+        {/* <Routes>
+        <Route exact path="/" element={<LoginPage></LoginPage>}/>
          <Route exact path="ScreenOne" element={<ScreenOne></ScreenOne>}></Route>
          <Route exact path='BudgetPensin' element = {<BudgetPensin></BudgetPensin>}></Route>
          <Route exact path='AccrualPension' element = {<AccrualPension></AccrualPension>}></Route>
          <Route exact path='BudgetPensionForSeniorSalary' element = {<BudgetPensionForSeniorSalary></BudgetPensionForSeniorSalary>}></Route>
-         <Route exact path='GeneralData' element = {<GeneralData></GeneralData>}></Route> */}
-         </Routes>
+         <Route exact path='GeneralData' element = {<GeneralData></GeneralData>}></Route>
+         </Routes> */}
       </BrowserRouter>
 
      
--- client/src/RetirementSimulator/Header.js
+++ client/src/RetirementSimulator/Header.js
@@ -1,10 +1,26 @@
+import './Header.css';
+import logo from "../img/לבן לוגו מיכל לוי (1) 2.png"
 function Header() {
 
     return (
         <div className='header'>
-           <div className="rec79"> 
-           
-           </div>
+            <div className="rec79">
+                <img className="logo-header" src={logo}></img>
+                <div className="frame39">
+                    <div className="click-to-home-page">
+                        מעבר לדף הבית
+                    </div>
+                </div>
+                <div className="group-289327">
+                    <img className="vector-phone" src="src\img\phone.png"></img>
+                    <p className="contact-us"> צור קשר </p>
+                </div>
+                <div className="line11"></div>
+                <div>
+                    {/* <img>אין איקון נורמלי...</img> */}
+                    <p className="welcom">היי {}</p>
+                </div>
+            </div>
         </div>
     );
 }
