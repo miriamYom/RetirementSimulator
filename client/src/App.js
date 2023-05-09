66e3bdd263f153052a6d87e106af110591591f58
YehuditShrg <y0548562311@outlook.co.il>
2023-05-09 12:55:14 +0300

    start the headaer... :(

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 // דע כי ה... הוא הנותן... לעשות חיל
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './RetirementSimulator/LoginPage';
-import Api from './RetirementSimulator/Api';
-import ScreenOne from './RetirementSimulator/UserSee/ScreenOne';
-import CreateEmployee from './RetirementSimulator/CreateEmployee';
-import BudgetPensin from './RetirementSimulator/UserSee/BudgetPension';
-import AccrualPension from './RetirementSimulator/UserSee/AccrualPension';
-import BudgetPensionForSeniorSalary from './RetirementSimulator/UserSee/BudgetPensionForSeniorSalary';
-import GeneralData from './RetirementSimulator/UserSee/GeneralData';
-
+// import LoginPage from './RetirementSimulator/LoginPage';
+// import Api from './RetirementSimulator/Api';
+// import ScreenOne from './RetirementSimulator/UserSee/ScreenOne';
+// import CreateEmployee from './RetirementSimulator/CreateEmployee';
+// import BudgetPensin from './RetirementSimulator/UserSee/BudgetPension';
+// import AccrualPension from './RetirementSimulator/UserSee/AccrualPension';
+// import BudgetPensionForSeniorSalary from './RetirementSimulator/UserSee/BudgetPensionForSeniorSalary';
+// import GeneralData from './RetirementSimulator/UserSee/GeneralData';
+import Header from './RetirementSimulator/Header';
 
 function App() {
   return (
@@ -16,14 +16,14 @@ function App() {
      <BrowserRouter>
      {/* <Api></Api> */}
      {/* <CreateEmployee></CreateEmployee> */}
-     
+     <Header></Header>
         <Routes>
-        <Route exact path="/" element={<LoginPage></LoginPage>}/>
+        {/* <Route exact path="/" element={<LoginPage></LoginPage>}/>
          <Route exact path="ScreenOne" element={<ScreenOne></ScreenOne>}></Route>
          <Route exact path='BudgetPensin' element = {<BudgetPensin></BudgetPensin>}></Route>
          <Route exact path='AccrualPension' element = {<AccrualPension></AccrualPension>}></Route>
          <Route exact path='BudgetPensionForSeniorSalary' element = {<BudgetPensionForSeniorSalary></BudgetPensionForSeniorSalary>}></Route>
-         <Route exact path='GeneralData' element = {<GeneralData></GeneralData>}></Route>
+         <Route exact path='GeneralData' element = {<GeneralData></GeneralData>}></Route> */}
          </Routes>
       </BrowserRouter>
 
---
+++ client/src/RetirementSimulator/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+
+    return (
+        <div className='header'>
+           <div className="rec79"> 
+           
+           </div>
+        </div>
+    );
+}
+export default Header;
