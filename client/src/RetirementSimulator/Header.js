234b977e711677eff12c56a68cc1f41dc86429cf
YehuditShrg <y0548562311@outlook.co.il>
2023-05-09 20:26:31 +0300

    the header is copped but isnt coplete

--- client/src/RetirementSimulator/Header.js
+++ client/src/RetirementSimulator/Header.js
@@ -1,26 +1,34 @@
 import './Header.css';
-import logo from "../img/לבן לוגו מיכל לוי (1) 2.png"
+import logo from "../img/לבן לוגו מיכל לוי (1) 2.png";
+import phoneIcon from "../img/phone.png";
+import personIcon1 from "../img/person (1).png";
+import personIcon2 from "../img/person (2).png";
 function Header() {
 
     return (
         <div className='header'>
-            <div className="rec79">
-                <img className="logo-header" src={logo}></img>
-                <div className="frame39">
-                    <div className="click-to-home-page">
-                        מעבר לדף הבית
-                    </div>
-                </div>
-                <div className="group-289327">
-                    <img className="vector-phone" src="src\img\phone.png"></img>
-                    <p className="contact-us"> צור קשר </p>
-                </div>
-                <div className="line11"></div>
-                <div>
-                    {/* <img>אין איקון נורמלי...</img> */}
-                    <p className="welcom">היי {}</p>
+            <div className="rec79"></div>
+
+            <div className="frame39">
+                <a className="click-to-home-web" href=''>
+                    מעבר לדף הבית
+                </a>
+            </div>
+            <div className="group-289328">
+                <p className="welcom">היי { }</p>
+                <div className='group-welcom'>
+                    <img className='personIcon1' src={personIcon1}></img>
+                    <img className='personIcon2' src={personIcon2}></img>
                 </div>
             </div>
+            <div className="group-289327">
+                <div className='group'>
+                    <img className="vector-phone" src={phoneIcon}></img>
+                </div>
+                <p className="contact-us"> צור קשר </p>
+            </div>
+            <img className="logo-header" src={logo}></img>
+            <div className="line11"></div>
         </div>
     );
 }
