54ba94d42d5e5c0117e203ae1fd2accaefa2c438
miriamYom <m0527669018@gmail.com>
2023-05-10 16:05:36 +0300

    קצת סדר בבלגן, ותפילה גדולה על פיגמה ועל המירג'וג'

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 // דע כי ה... הוא הנותן... לעשות חיל
+import "@fontsource/rubik"; 
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import LoginPage from './RetirementSimulator/Else/LoginPage';
 // import Api from './RetirementSimulator/Else/Api';
@@ -10,6 +12,7 @@ import BudgetPensionForSeniorSalary from './RetirementSimulator/UserSee/BudgetPe
 import GeneralData from './RetirementSimulator/UserSee/GeneralData';
 import Header from './RetirementSimulator/Header';
 import PreviousNext from './RetirementSimulator/PreviousNext';
+import Salary from "./RetirementSimulator/UserSee/ScreenThree/Salary";
 
 function App() {
   return (
@@ -18,14 +21,16 @@ function App() {
      {/* <Api></Api> */}
      {/* <CreateEmployee></CreateEmployee> */}
      <Header></Header>
-        {/* <Routes>
-        <Route exact path="/" element={<LoginPage></LoginPage>}/>
-         <Route exact path="ScreenOne" element={<ScreenOne></ScreenOne>}></Route>
+        <Routes>
+        {/* <Route exact path="/" element={<LoginPage></LoginPage>}/> */}
+         {/* <Route exact path="ScreenOne" element={<ScreenOne></ScreenOne>}></Route> */}
          <Route exact path='BudgetPensin' element = {<BudgetPensin></BudgetPensin>}></Route>
          <Route exact path='AccrualPension' element = {<AccrualPension></AccrualPension>}></Route>
          <Route exact path='BudgetPensionForSeniorSalary' element = {<BudgetPensionForSeniorSalary></BudgetPensionForSeniorSalary>}></Route>
          <Route exact path='GeneralData' element = {<GeneralData></GeneralData>}></Route>
-         </Routes> */}
+         <Route exact path="Salary" element = {<Salary></Salary>}></Route>
+         </Routes>
+         <PreviousNext></PreviousNext>
       </BrowserRouter>
      
     </div>
---
+++ client/src/RetirementSimulator/UserSee/ScreenTwo/PersonalData.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from "yup";
+
+function PersonalData() {
+
+const onSubmit = (data) => {
+    console.log(data);
+}
+
+const schema = yup.object().shape({
+    name: yup.string().required(),
+    id: yup.string().required(),
+    birthDate: yup.date(),//min, max...
+    startWorkDate: yup.date(),
+    retirementDate: yup.date().required(),
+    // reason:  enum
+    //isClothingForAudienceMembers: radio button
+    // IsThreeLevel: radio
+    // monthOfClothingPayment: enum
+    // IsCurrentYear: radio
+    //IsMonthlyRecoveryPayment: radio
+    numberOfDaysOfRecoveryToBePaid: yup.number().integer().min(8).max(13).required(),
+    //recoveryPaymentMonth: enum
+
+});
+
+const { register, handleSubmit, formState: {errors} } = useForm({
+    resolver: yupResolver(schema),
+});
+
+
+
+    const [reason, setReason] = useState('retirementAge');
+
+    const Reason = e => {
+        setReason(e.target.value);
+    }
+
+
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+            <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <center>
+                    <input name="name" {...register('name')} placeholder="שם העובד"></input>
+                    <br></br>
+                    <input name="id" {...register('id')} placeholder="תעודת זהות"></input>
+                    <h4>תאריך לידה</h4>
+                    <input name="birthDate" {...register('birthDate')} type="date"></input>
+                    <h4>תאריך תחילת עבודה</h4>
+                    <input name="startWorkDate" {...register('startWorkDate')} type="date"></input>
+                    <h4>תאריך פרישה צפוי</h4>
+                    <input name="retirementDate" {...register('retirementDate')} type="date"></input>
+                    <h4>סוג הפרישה</h4>
+                    <select name="reason" {...register("reason")}
+                        onChange={Reason}>
+                        <option value="retirementAge">גיל פרישה</option>
+                        <option value="resignation">התפטרות</option>
+                        <option value="dismissal">פיטורין</option>
+                        <option value="retirementForHealthReasons">סיבה רפואית</option>
+                        <option value="death">מוות</option>
+                    </select>
+
+                    <br></br>
+                    {reason === "dismissal" ?
+                        (<>
+                            <h4>חלף הודעה מוקדמת</h4>
+                            <select name="advanceNotice" {...register("advanceNotice")}>
+                                <option value="notEligible">לא זכאי</option>
+                                <option value="month">חודש</option>
+                                <option value="twoMonths">חודשיים</option>
+                                <option value="treeMonths">שלושה חודשים</option>
+                            </select>
+                        </>) : null}
+                    </center>
+                <input type="submit"></input>
+                {/* מצב משפחתי */}
+            </form>
+           
+        </>
+    )
+}
+export default PersonalData;
