f6104f8c9b8e52832318524cdec9d143d3de1824
miriamYom <m0527669018@gmail.com>
2023-06-04 17:27:57 +0300

    Get All is work...

--- client/src/RetirementSimulator/Admin/GetAllUsers.js
+++ client/src/RetirementSimulator/Admin/GetAllUsers.js
@@ -6,54 +6,68 @@ import { useRef } from 'react';
 export default function GetAllUsers() {
 
     const allUsers = useRef();
-    allUsers.current = {};
-    // allUsers.current = [
-    //     { "name": 'Baruch Shraga', "role": 'Yeshiva bocher', "email": 'm025328642@gmail.com', "phoneNumber": '0548546224', "password": '6666' },
-    //     { "name": 'Bibi', "role": 'Prime Minister', "email": 'bibist@bib', "phoneNumber": '11111111', "password": '12345' }
-    // ];
-    let [flag, setflag] = useState(false);
+    // allUsers.current = {};
+
+    const [flag, setflag] = useState(false);
     function getAll() {
         setflag(true);
         axios.get('https://localhost:7049/RentiermentSimulator/GetAll')
-            .then((data) => {
-                console.log(data);
-                console.log(JSON.stringify(allUsers.current) === '{}');
-                allUsers.current = data.data;
-                console.log(allUsers.current[0]);
-                console.log(allUsers);
-                console.log(JSON.stringify(allUsers.current) === '{}');
+            .then((response) => {
+
+                allUsers.current = response.data;
+
             });
     }
 
+
     return (
         <>
-            <button disabled={flag} onClick={getAll}>קבל פרטי כל המנויים</button>
-            {/* { 
-                 JSON.stringify(allUsers.current) === '{}' ?
-                    null :*/}
+
+            <button /*disabled={flag}*/ onClick={getAll}>קבל פרטי כל המנויים</button>
             <table>
                 <tr>
-                    <th>name</th>
-                    <th>role</th>
-                    <th>email</th>
-                    <th>password</th>
-                    <th>phone number</th>
-                    <th>subscriptionPeriodDate</th>
-                    {/* <th>is sub?</th> */}
+                    <th>שם</th>
+                    <th>תפקיד</th>
+                    <th>כתובת מייל</th>
+                    <th>סיסמה</th>
+                    <th>טלפון</th>
+                    <th>תוקף מנוי</th>
                 </tr>
-                {Object.keys(allUsers.current).map((item, i) => (
-                    <tr>
+            </table>
+            {/* {allUsers.current.map((item, index) => (
+                    <tr key={index}>
                         <td>{item.name}</td>
                         <td>{item.role}</td>
                         <td>{item.email}</td>
                         <td>{item.password}</td>
                         <td>{item.phoneNumber}</td>
                         <td>{item.subscriptionPeriodDate}</td>
-                        {/* <th>{item.subscriptionPeriodDate > new Date()}</th> */}
                     </tr>
-                ))}
-            </table>
-            {/* } */}
+                ))} */}
+
+
+            
+                {Array.isArray(allUsers.current) ? (
+                    allUsers.current.map((item, index) => (
+                        <tr key={index}>
+                            <td> {item.name}</td>
+                            <td>{item.role}</td>
+                            <td> {item.email}</td>
+                            <td>{item.password}</td>
+                            <td>{item.phoneNumber}</td>
+                            <td> {item.subscriptionPeriodDate}</td>
+                        </tr>
+                    ))
+                ) : (
+                    <p>לא נמצאו משתמשים</p>
+                )}
+            
+
+
+
+
+
         </>
     );
 }
+
