b8c70397c072a9864cd800b8903f86a478c5895a
miriamYom <m0527669018@gmail.com>
2023-06-08 16:14:09 +0300

    Delete.... OMG

--- client/src/RetirementSimulator/Admin/DeleteUser.js
+++ client/src/RetirementSimulator/Admin/DeleteUser.js
@@ -5,22 +5,29 @@ import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
 function DeleteUser() {
-    const schema = yup.object().shape({
-        name: yup.string().default(""),//.required(),
-        role: yup.string().default(""),//.required(),
-        email: yup.string().email().required('Email is required'),//'Invalid email'
-        phoneNumber: yup.number().default(""),//.required()
-        // subscriptionPeriodDate: /*JSON.parse*/(yup.date()).toJSON()//.required()
-    });
+    // const schema = yup.object().shape({
+    //     name: yup.string().default(""),//.required(),
+    //     role: yup.string().default(""),//.required(),
+    //     email: yup.string().email().required('Email is required'),//'Invalid email'
+    //     phoneNumber: yup.number().default(""),//.required()
+    //     subscriptionPeriodDate: new Date().toJSON()
+    //     // subscriptionPeriodDate: /*JSON.parse*/(yup.date()).toJSON()//.required()
+    // });
+
 
     const { register, handleSubmit, formState: { errors }, reset, getValues } = useForm({
-        resolver: yupResolver(schema),
+        defaultValues: {
+            name:"",
+            subscriptionPeriodDate: new Date().toJSON()
+          },
+
+        // resolver: yupResolver(schema),
     });
 
     const onSubmit = (data) => {
         console.log(data);
-        axios.delete("https://localhost:7049/RentiermentSimulator/DeleteUser", data)
-            .then(response => console.log(response))
+        axios.delete("https://localhost:5170/RentiermentSimulator/DeleteUser", data)
+            .then(response => console.log(response.data))
             .catch((error) => console.log(error));
     };
 
@@ -30,7 +37,36 @@ function DeleteUser() {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <label>כתובת מייל:</label>
                 <input {...register("email")} placeholder="email" type="email" ></input>
-                <p style={{ "color": "red" }}>{errors.email?.message}</p>
+                {/* <p style={{ "color": "red" }}>{errors.email?.message}</p> */}
+
+
+
+
+                <label>שם:</label>
+                <input {...register("name")} type="text" />
+                {/* <p style={{ "color": "red" }}>{errors.name?.message}</p> */}
+
+                <label>תפקיד:</label>
+                <input {...register("role")} type="text" />
+                {/* <p style={{ "color": "red" }}>{errors.role?.message}</p> */}
+
+
+                <label>טלפון:</label>
+                <input {...register("phoneNumber")} type="text" />
+                {/* <p style={{ "color": "red" }}>{errors.phoneNumber?.message}</p> */}
+
+                <label>סיסמה:</label>
+                <input {...register("password")} type="password" />
+                {/* <p style={{ "color": "red" }}>{errors.password?.message}</p> */}
+
+                <label>תוקף מנוי:</label>
+                <input {...register("subscriptionPeriodDate")} type="text" />
+                {/* <p style={{ "color": "red" }}>{errors.subscriptionPeriodDate?.message}</p> */}
+
+
+
+
+
                 <button type="submit">Submit</button>
             </form>
         </>
